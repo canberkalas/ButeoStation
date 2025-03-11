d9a5914e86f6c4dac01fe891b6d2218f3c876614
canberkalas <canberkalas@gmail.com>
2025-03-11 12:33:37 +0300

    Update satt.rs

--- satt.rs
+++ satt.rs
@@ -1,7 +1,33 @@
-use reqwest;
+use std::net::UdpSocket;
+use aes::Aes128;
+use block_modes::{BlockMode, Cbc};
+use block_modes::block_padding::Pkcs7;
 
-pub fn fetch_satellite_data(coordinates: &str) -> Result<String, reqwest::Error> {
-    let api_url = format!("https://api.sentinel-hub.com/ogc/wms/{}", coordinates);
-    let response = reqwest::blocking::get(&api_url)?;
-    Ok(response.text()?)
+type Aes128Cbc = Cbc<Aes128, Pkcs7>;
+
+pub fn encrypt_data(key: &[u8; 16], iv: &[u8; 16], data: &[u8]) -> Vec<u8> {
+    let cipher = Aes128Cbc::new_from_slices(key, iv).unwrap();
+    cipher.encrypt_vec(data)
+}
+
+pub fn decrypt_data(key: &[u8; 16], iv: &[u8; 16], ciphertext: &[u8]) -> Vec<u8> {
+    let cipher = Aes128Cbc::new_from_slices(key, iv).unwrap();
+    cipher.decrypt_vec(ciphertext).unwrap()
+}
+
+pub fn send_data_via_satellite(key: &[u8; 16], iv: &[u8; 16], data: &[u8]) {
+    let encrypted_data = encrypt_data(key, iv, data);
+    
+    let socket = UdpSocket::bind("0.0.0.0:34254").expect("Could not bind to address");
+    socket.send_to(&encrypted_data, "satellite.endpoint:34254").expect("Could not send data");
+}
+
+pub fn receive_data_via_satellite(key: &[u8; 16], iv: &[u8; 16]) -> Vec<u8> {
+    let socket = UdpSocket::bind("satellite.endpoint:34254").expect("Could not bind to address");
+    let mut buffer = [0u8; 512];
+    
+    let (amt, _src) = socket.recv_from(&mut buffer).expect("Could not receive data");
+    let encrypted_data = &buffer[..amt];
+    
+    decrypt_data(key, iv, encrypted_data)
 }
